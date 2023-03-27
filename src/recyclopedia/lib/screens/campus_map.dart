d4443433a53102cd5d0368bdd0729696a99e0617
fjgao <fjgao@bu.edu>
2023-03-27 12:38:30 -0400

    rename file

--- src/recyclopedia/lib/screens/campus_map.dart
+++ src/recyclopedia/lib/screens/campus_map.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recyclopedia/main.dart';
 
-class MapPage extends StatelessWidget {
+class CampusMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
