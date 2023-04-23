df7012f56deed95dc821040bd37f72817436e154
fjgao <fjgao@bu.edu>
2023-04-23 02:40:31 -0400

    completed functionality of location modal has some issues in styling

--- src/recyclopedia/lib/main.dart
+++ src/recyclopedia/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:recyclopedia/all.dart';
 import 'package:recyclopedia/providers/assets_provider.dart';
 import 'package:recyclopedia/providers/category_provider.dart';
+import 'package:recyclopedia/providers/location_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => ResourcesList()),
         ChangeNotifierProvider(create: (context) => CategoryProvider()),
         ChangeNotifierProvider(create: (context) => AssetsProvider()),
+        ChangeNotifierProvider(create: (context) => LocationProvider())
       ],
       child: MaterialApp(
         title: 'Namer App',
